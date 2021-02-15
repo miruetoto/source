a320bc1ae67b8abbbc6ca6675b74907452d1aba0
miruetoto <44726070+miruetoto@users.noreply.github.com>
2021-02-15 09:54:59 +0900

    .

--- .ipynb_checkpoints/pybase-checkpoint.py
+++ .ipynb_checkpoints/pybase-checkpoint.py
@@ -18,6 +18,19 @@ import rpy2.robjects as ro
 #ro.r('library(tidyverse)')
 
 ############################## rpy2 functions ##############################
+def pull(r):
+    return r2p(ro.globalenv[r])
+
+def push(py,rname=None):
+    import inspect
+    def retrieve_name(var):
+        for fi in reversed(inspect.stack()):
+            names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
+            if len(names) > 0:
+                return names[0]
+    if rname==None: rname = retrieve_name(py)
+    ro.globalenv[rname]=p2r(py)
+    
 def p2r(A):
     from rpy2.robjects.vectors import FloatVector 
     from rpy2.robjects.vectors import StrVector as s2r_temp
@@ -32,9 +45,7 @@ def p2r(A):
         Acopy=A.T.copy()
         nrow=Acopy.shape[0]
         Acopy.shape=(np.prod(Acopy.shape),1)
-        rtn=ro.r.matrix(a2r_temp(m2a(Acopy)),ncol=nrow)
-        del(Acopy)
-        ro.globalenv['A']=rtn
+        rtn=ro.r.matrix(a2r_temp(list(Acopy)),ncol=nrow)
         return rtn
 
     from rpy2.robjects import pandas2ri
@@ -45,9 +56,9 @@ def p2r(A):
             rtn = ro.conversion.py2rpy(A)
         return rtn
     
-    if type(A)==type(initpd('0',n=2,p=2)):
+    if type(A)==type(pd.DataFrame(np.zeros([2,2]))):
         rtn=pd2r_temp(A) 
-    elif type(A)==type(init('0',(2,2))):
+    elif type(A)==type(np.zeros([2,2])):
         rtn=m2r_temp(A)
     elif type(A)==str: #  elif type(pd.DataFrame(np.matrix(A)).iloc[0,0])==str: 와 순서바꾸면 안됨
         rtn=s2r_temp(A)        
@@ -55,17 +66,7 @@ def p2r(A):
         rtn=s2r_temp(pd.DataFrame(np.matrix(A)).T.iloc[:,0])
     else:
         rtn=a2r_temp(A)
-    return rtn 
-
-def push(py,rname=None):
-    import inspect
-    def retrieve_name(var):
-        for fi in reversed(inspect.stack()):
-            names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
-            if len(names) > 0:
-                return names[0]
-    if rname==None: rname = retrieve_name(py)
-    ro.globalenv[rname]=p2r(py)
+    return rtn
 
 def r2p(A):
     from rpy2.robjects import pandas2ri
@@ -80,7 +81,7 @@ def r2p(A):
     def r2pd_temp(A):
         with localconverter(ro.default_converter + pandas2ri.converter):
             rtn = ro.conversion.rpy2py(A)
-        return rtn        
+        return rtn   
     
     ro.globalenv['temp']=A
     if ro.r('is.null(dim(temp))')[0]==False: ## in the cases of matrix or dataframe
@@ -95,40 +96,6 @@ def r2p(A):
     ro.r('rm("temp")')
     return rtn
 
-def pull(r):
-    return r2p(ro.globalenv[r])
-
-
-
-############################## python functions ##############################
-def cc(start=0,end=1,samplingFreq=1):
-    rtn=m2a(cbind(np.arange(start,end,1/samplingFreq),end).T)
-    if start==int(start) and end==int(end) and samplingFreq==1: rtn=np.array(list(map(int,rtn)))
-    return rtn
-
-def co(start=0,end=1,samplingFreq=1):
-    rtn=m2a(np.asmatrix(np.arange(start,end,1/samplingFreq)).T)
-    if start==int(start) and end==int(end) and samplingFreq==1: rtn=np.array(list(map(int,rtn)))
-    return rtn
-
-def oc(start=0,end=1,samplingFreq=1):
-    rtn=m2a(np.asmatrix(np.arange(start,end,1/samplingFreq)).T+1/samplingFreq)
-    if start==int(start) and end==int(end) and samplingFreq==1: rtn=np.array(list(map(int,rtn)))
-    return rtn 
-
-def oo(start=0,end=1,samplingFreq=1):
-    rtn=np.asmatrix(np.arange(start,end,1/samplingFreq)).T
-    rtn=m2a(rtn[1:len(rtn)])
-    if start==int(start) and end==int(end) and samplingFreq==1: rtn=np.array(list(map(int,rtn)))
-    return rtn 
-
-### 입력은 n×p matrix , 혹은 n×p DataFrame 으로 바꿀 수 있는 어떤자료 / 출력 n×p np.matrix 임. 이때 첫번째 row가 0임. 
-def lagg(inputMatrix,lag):
-    inputdf=pd.DataFrame(inputMatrix)
-    rtn=np.asmatrix(inputdf.shift(lag))
-    rtn[range(lag),:]=0
-    return rtn
-
 def cbind(*Mat):
     lenofMat=len(Mat)
     if lenofMat==1: 
@@ -138,9 +105,9 @@ def cbind(*Mat):
         rtn=cbindtemp(Mat[0],Mat[1])
     else: 
         rtn=cbindtemp(Mat[0],Mat[1])
-        for i in co(2,lenofMat):
+        for i in np.arange(2,lenofMat):
             rtn=cbindtemp(rtn,Mat[i])
-    return rtn 
+    return rtn
 
 def rbind(*Mat):
     lenofMat=len(Mat)
@@ -151,7 +118,7 @@ def rbind(*Mat):
         rtn=rbindtemp(Mat[0],Mat[1])
     else: 
         rtn=rbindtemp(Mat[0],Mat[1])
-        for i in co(2,lenofMat):
+        for i in np.arange(2,lenofMat):
             rtn=rbindtemp(rtn,Mat[i])
     return rtn
 
@@ -239,7 +206,7 @@ def rbindtemp(A,B):
     
     return np.vstack([A,B])
 
-def ifo(A):
+def info(A):
     print("type of data  : ",type(A))
     try: len(A)
     except TypeError as e: print("len of data   : ",e)
@@ -343,7 +310,7 @@ def sprod(*index):
         else: indexListtype[i]=index[i]
         
     mindex=pd.MultiIndex.from_product(indexListtype)
-    val=init("0",mindex.shape[0])
+    val=np.zeros(mindex.shape[0])
     data=pd.DataFrame(val,index=mindex).reset_index()
     mindextable=data.iloc[:,0:data.shape[1]-1]
     rtn=['']*mindextable.shape[0]
--- hst.py
+++ hst.py
@@ -46,7 +46,7 @@ def hst(f,W,V,τ,b,γ=0.5,T=999999999): #samefunction with hst1realization excep
     hst_results=pd.DataFrame(data={'Nodename(=v)':V,'h0':f}) 
     
     # 3. h^1 ~ h^τ
-    for ℓ in cc(1,τ): 
+    for ℓ in np.arange(1,τ+1): 
         print('\r'+str(ℓ)+'/'+str(τ),sep='',end='')
         Wthreshed=W>np.asmatrix(np.random.random((n,n)))
         hstwalkrslt=hst_onewalk(h=hst_results['h'+str(ℓ-1)],W=Wthreshed,b=b*γ**flowcount[ℓ-1],
@@ -58,188 +58,185 @@ def hst(f,W,V,τ,b,γ=0.5,T=999999999): #samefunction with hst1realization excep
     
     return hst_results
 
-def hhmat(hstresult):
-    τ=int((hstresult.shape[1]-2))
-    rtn=np.asmatrix(hstresult[sprod('h',cc(0,τ))])
+# def hmat(hstresult):
+#     τ=int((hstresult.shape[1]-2))
+#     rtn=np.asmatrix(hstresult[sprod('h',np.arange(0,τ+1))])
+#     return rtn
+
+def hmat(hst_):
+    τ=int((hst_.shape[1]-2))
+    rtn=np.asmatrix(hst_[sprod('h',np.arange(0,τ+1))])
+    rtn=rtn-np.matrix(np.apply_along_axis(np.mean,1,rtn)).T
     return rtn
 
-def hmat(hstresult):
-    τ=int((hstresult.shape[1]-2))
-    rtn=np.asmatrix(hstresult[sprod('h',cc(0,τ))])
-    rtn=rtn-apply(rtn,"np.mean",arrow="right")
-    return rtn
-
-def L2dist(hhlike,prnt=False): #supporting snowdist, #hh:=n*p 
-    hhlike=np.array(hhlike)
-    n=len(hhlike)
-    rtn=np.array(init('0',(n,n)))
+def sigmamat(hmat_): #supporting snowdist, #hh:=n*p 
+    hmat_=np.array(hmat_)
+    n=len(hmat_)
+    rtn=np.array(np.zeros([n,n]))
     try: 
-        rtn=np.sum((hhlike[:,np.newaxis,:]-hhlike[np.newaxis,:,:])**2,axis=-1)
+        rtn=np.sum((hmat_[:,np.newaxis,:]-hmat_[np.newaxis,:,:])**2,axis=-1)
     except MemoryError:
-        if prnt==True: print('calculating snowdistance serially(due to lack of memory)')
-        for i in co(0,n):
-            rtn[i,:]=np.sum((hhlike[i,:]-hhlike[:,:])**2,axis=1)
-            if prnt==True: print('\r'+str(i),'/'+str(n),sep='',end='')
-        if prnt==True: print('\n'+'end')
+        for i in np.arange(0,n):
+            rtn[i,:]=np.sum((hmat_[i,:]-hmat_[:,:])**2,axis=1)
     return np.asmatrix(rtn)
 
-def Sigma(hh,τmax=None,prnt=False): 
-    if τmax==None: rtn=L2dist(hh,prnt=prnt)
-    else: rtn=L2dist(hh[:,0:(τmax+1)],prnt=prnt)
-    return rtn
+# def sigmamat_v2(hmat_,τmax=None): 
+#     if τmax==None: rtn=l2dist(hmat_)
+#     else: rtn=l2dist(hmat_[:,0:(τmax+1)])
+#     return rtn
 
-def dist2W(dist,θ=1): 
-    n=len(dist)
-    rtn=init('0',(n,n))
-    for i in co(0,n):
-        for j in co(0,n):
+def weightmat(sigmamat_,θ=1): 
+    n=len(sigmamat_)
+    rtn=np.zeros([n,n])
+    for i in np.arange(0,n):
+        for j in np.arange(0,n):
             if i==j: rtn[i,j]=0
-            else: rtn[i,j]=np.exp(-dist[i,j]**2/(2*θ**2))
+            else: rtn[i,j]=np.exp(-sigmamat_[i,j]**2/(2*θ**2))
     return rtn
 
-def total_degree(W):
-    return np.sum(m2a(apply(W,'np.sum')))
+# def total_degree(W):
+#     return np.sum(W)
 
-### 2. hst: visualization 
-def datavis4ts(f,nodename=None,groupindex=None,
-           figname='temp',figsize=(1,1),dpi=1,cex=1,
-           text=None,fade=0.5):
-    n=len(f)
+# ### 2. hst: visualization 
+# def datavis4ts(f,nodename=None,groupindex=None,
+#            figname='temp',figsize=(1,1),dpi=1,cex=1,
+#            text=None,fade=0.5):
+#     n=len(f)
     
-    if groupindex==None: colors=[0]*n
-    elif groupindex=='continuous': colors=cm.rainbow(np.linspace(1, 0, n))
-    else: colors=np.array(groupindex)
+#     if groupindex==None: colors=[0]*n
+#     elif groupindex=='continuous': colors=cm.rainbow(np.linspace(1, 0, n))
+#     else: colors=np.array(groupindex)
     
-    f=np.array(f)
-    figsize=(10*figsize[0],6*figsize[1])
-    dpi=150*dpi
-    cex=50*cex
-    if text!=None: text=10*text
-    fade=fade
+#     f=np.array(f)
+#     figsize=(10*figsize[0],6*figsize[1])
+#     dpi=150*dpi
+#     cex=50*cex
+#     if text!=None: text=10*text
+#     fade=fade
     
-    Fig=plt.figure(figsize=figsize, dpi=dpi) # Make figure object 
-    ax=plt.axes()
-    plt.scatter(cc(1,n),f,c=colors,s=cex,alpha=fade)
-    style=dict(size=text,color='k')
+#     Fig=plt.figure(figsize=figsize, dpi=dpi) # Make figure object 
+#     ax=plt.axes()
+#     plt.scatter(np.arange(1,n+1),f,c=colors,s=cex,alpha=fade)
+#     style=dict(size=text,color='k')
     
-    if nodename==None:
-        for i in cc(1,n): 
-            if text!=None: ax.text(i,f[i-1],'%s'% str(i), **style) # numbering index of nodes 
-        rtn=Fig 
-    else: 
-        for i in cc(1,n): 
-            if text!=None: ax.text(i,f[i-1],'%s'% nodename[i-1], **style) # numbering index of nodes 
-        rtn=Fig 
-    rtn.savefig(figname+'.pdf')
+#     if nodename==None:
+#         for i in np.arange(1,n+1): 
+#             if text!=None: ax.text(i,f[i-1],'%s'% str(i), **style) # numbering index of nodes 
+#         rtn=Fig 
+#     else: 
+#         for i in np.arange(1,n+1): 
+#             if text!=None: ax.text(i,f[i-1],'%s'% nodename[i-1], **style) # numbering index of nodes 
+#         rtn=Fig 
+#     rtn.savefig(figname+'.pdf')
 
-def datavis4sct(v1,v2,nodename=None,groupindex=None,
-           figname='temp',figsize=(1,1),dpi=1,cex=1,
-           text=None,fade=0.5):
-    n=len(v1)
+# def datavis4sct(v1,v2,nodename=None,groupindex=None,
+#            figname='temp',figsize=(1,1),dpi=1,cex=1,
+#            text=None,fade=0.5):
+#     n=len(v1)
     
-    if groupindex==None: colors=[0]*n
-    elif groupindex=='continuous': colors=cm.rainbow(np.linspace(1, 0, n))
-    else: colors=np.array(groupindex)
+#     if groupindex==None: colors=[0]*n
+#     elif groupindex=='continuous': colors=cm.rainbow(np.linspace(1, 0, n))
+#     else: colors=np.array(groupindex)
     
-    v1=np.array(v1)
-    v2=np.array(v2)
-    figsize=(10*figsize[0],6*figsize[1])
-    dpi=150*dpi
-    cex=50*cex
-    if text!=None: text=10*text
-    fade=fade
+#     v1=np.array(v1)
+#     v2=np.array(v2)
+#     figsize=(10*figsize[0],6*figsize[1])
+#     dpi=150*dpi
+#     cex=50*cex
+#     if text!=None: text=10*text
+#     fade=fade
     
-    Fig=plt.figure(figsize=figsize, dpi=dpi) # Make figure object 
-    ax=plt.axes()
-    plt.scatter(v1,v2,c=colors,s=cex,alpha=fade)
-    style=dict(size=text,color='k')
+#     Fig=plt.figure(figsize=figsize, dpi=dpi) # Make figure object 
+#     ax=plt.axes()
+#     plt.scatter(v1,v2,c=colors,s=cex,alpha=fade)
+#     style=dict(size=text,color='k')
     
-    if nodename==None:
-        for i in cc(1,n): 
-            if text!=None: ax.text(v1[i-1],v2[i-1],'%s'% str(i), **style) # numbering index of nodes 
-        rtn=Fig 
-    else: 
-        for i in cc(1,n): 
-            if text!=None: ax.text(v1[i-1],v2[i-1],'%s'% nodename[i-1], **style) # numbering index of nodes 
-        rtn=Fig 
-    rtn.savefig(figname+'.pdf')
+#     if nodename==None:
+#         for i in np.arange(1,n+1): 
+#             if text!=None: ax.text(v1[i-1],v2[i-1],'%s'% str(i), **style) # numbering index of nodes 
+#         rtn=Fig 
+#     else: 
+#         for i in np.arange(1,n+1): 
+#             if text!=None: ax.text(v1[i-1],v2[i-1],'%s'% nodename[i-1], **style) # numbering index of nodes 
+#         rtn=Fig 
+#     rtn.savefig(figname+'.pdf')
 
-def pca4vis3d(Σ,nodename=None,groupindex=None,
-           figname='temp',title=None,figsize=(1,1),dpi=1,cex=1,
-           xlim=None,ylim=None,zlim=None,text=None,fade=0.5,
-           prnt=False): # size=(size of obs representation, size of text which represent obs index)
+# def pca4vis3d(Σ,nodename=None,groupindex=None,
+#            figname='temp',title=None,figsize=(1,1),dpi=1,cex=1,
+#            xlim=None,ylim=None,zlim=None,text=None,fade=0.5,
+#            prnt=False): # size=(size of obs representation, size of text which represent obs index)
 
-    from sklearn.decomposition import PCA 
-    from mpl_toolkits import mplot3d
-    if prnt==True: print('PCA start')
-    pca=PCA(n_components=3) # PCA start 
-    n=Σ.shape[0]
-    pca.fit(Σ) 
-    pcarslt=pca.transform(Σ) # PCA end 
-    if prnt==True: print('end')
+#     from sklearn.decomposition import PCA 
+#     from mpl_toolkits import mplot3d
+#     if prnt==True: print('PCA start')
+#     pca=PCA(n_components=3) # PCA start 
+#     n=Σ.shape[0]
+#     pca.fit(Σ) 
+#     pcarslt=pca.transform(Σ) # PCA end 
+#     if prnt==True: print('end')
 
-    if groupindex==None: colors=['gray']*n
-    elif groupindex=='continuous': colors=cm.rainbow(np.linspace(1, 0, n))
-    else: colors=np.array(groupindex)
+#     if groupindex==None: colors=['gray']*n
+#     elif groupindex=='continuous': colors=cm.rainbow(np.linspace(1, 0, n))
+#     else: colors=np.array(groupindex)
     
-    figsize=(20*figsize[0],6*figsize[1])
-    dpi=200*dpi
-    cex=50*cex
-    if text!=None: text=10*text
-    fade=fade
+#     figsize=(20*figsize[0],6*figsize[1])
+#     dpi=200*dpi
+#     cex=50*cex
+#     if text!=None: text=10*text
+#     fade=fade
     
-    Fig=plt.figure(figsize=figsize, dpi=dpi)  # Make figure object 
-    ax=plt.axes(projection='3d') # define type of axes: 3d plot 
-    if title!=None: ax.text(0,0,zlim[1],title,fontsize=30)
-    if xlim!=None: ax.set_xlim(xlim[0],xlim[1])
-    if ylim!=None: ax.set_ylim(ylim[0],ylim[1])
-    if zlim!=None: ax.set_zlim(zlim[0],zlim[1])
-    ax.scatter3D(pcarslt[:,0],pcarslt[:,1],pcarslt[:,2],s=cex,c=colors,alpha=fade) # drawing each obs by scatter in 3d axes   
-    ax.ticklabel_format(style='sci', axis='x',scilimits=(0,0))
-    ax.ticklabel_format(style='sci', axis='y',scilimits=(0,0))
-    ax.ticklabel_format(style='sci', axis='z',scilimits=(0,0))
-    ax.xaxis.set_major_locator(plt.MaxNLocator(9))
-    ax.yaxis.set_major_locator(plt.MaxNLocator(5))
-    ax.zaxis.set_major_locator(plt.MaxNLocator(3))
-    ax.w_xaxis.pane.fill = False
-    ax.w_yaxis.pane.fill = False
-    ax.w_zaxis.pane.fill = False
-    if prnt==True: print('labeling (observation-wise)')
-    if nodename==None:
-        for i in cc(1,n): 
-            if prnt==True: print('\r'+str(i),'/'+str(n),sep='',end='')
-            if text!=None: ax.text(pcarslt[i-1,0],pcarslt[i-1,1],pcarslt[i-1,2],'%s'% (str(i)), size=text, zorder=1,color='k') # numbering index of nodes 
-        if prnt==True: print('\n'+'end')
-    else: 
-        for i in cc(1,n): 
-            if prnt==True: print('\r'+str(i),'/'+str(n),sep='',end='')
-            if text!=None: ax.text(pcarslt[i-1,0],pcarslt[i-1,1],pcarslt[i-1,2],'%s'% (nodename[i-1]), size=text, zorder=1,color='k') # numbering index of nodes 
-        if prnt==True: print('\n'+'end')
-    Fig.savefig(figname+'.pdf')
-    rtn=Fig
+#     Fig=plt.figure(figsize=figsize, dpi=dpi)  # Make figure object 
+#     ax=plt.axes(projection='3d') # define type of axes: 3d plot 
+#     if title!=None: ax.text(0,0,zlim[1],title,fontsize=30)
+#     if xlim!=None: ax.set_xlim(xlim[0],xlim[1])
+#     if ylim!=None: ax.set_ylim(ylim[0],ylim[1])
+#     if zlim!=None: ax.set_zlim(zlim[0],zlim[1])
+#     ax.scatter3D(pcarslt[:,0],pcarslt[:,1],pcarslt[:,2],s=cex,c=colors,alpha=fade) # drawing each obs by scatter in 3d axes   
+#     ax.ticklabel_format(style='sci', axis='x',scilimits=(0,0))
+#     ax.ticklabel_format(style='sci', axis='y',scilimits=(0,0))
+#     ax.ticklabel_format(style='sci', axis='z',scilimits=(0,0))
+#     ax.xaxis.set_major_locator(plt.MaxNLocator(9))
+#     ax.yaxis.set_major_locator(plt.MaxNLocator(5))
+#     ax.zaxis.set_major_locator(plt.MaxNLocator(3))
+#     ax.w_xaxis.pane.fill = False
+#     ax.w_yaxis.pane.fill = False
+#     ax.w_zaxis.pane.fill = False
+#     if prnt==True: print('labeling (observation-wise)')
+#     if nodename==None:
+#         for i in np.arange(1,n+1): 
+#             if prnt==True: print('\r'+str(i),'/'+str(n),sep='',end='')
+#             if text!=None: ax.text(pcarslt[i-1,0],pcarslt[i-1,1],pcarslt[i-1,2],'%s'% (str(i)), size=text, zorder=1,color='k') # numbering index of nodes 
+#         if prnt==True: print('\n'+'end')
+#     else: 
+#         for i in np.arange(1,n+1): 
+#             if prnt==True: print('\r'+str(i),'/'+str(n),sep='',end='')
+#             if text!=None: ax.text(pcarslt[i-1,0],pcarslt[i-1,1],pcarslt[i-1,2],'%s'% (nodename[i-1]), size=text, zorder=1,color='k') # numbering index of nodes 
+#         if prnt==True: print('\n'+'end')
+#     Fig.savefig(figname+'.pdf')
+#     rtn=Fig
 
-def pca4msvis3d(hstresult,τlist,
-              nodename=None,groupindex=None,
-              figname='temp',figsize=(1,1),dpi=1,cex=1,text=None,fade=1,
-              prnt=False,logscale=(False,False,False)): # size=(size of obs representation, size of text which represent obs index)
-    Σresult=τlist.copy()
-    hh=hmat(hstresult)
-    M=len(τlist)
-    from sklearn.decomposition import PCA 
-    pca=PCA(n_components=3) # PCA start 
-    pca.fit(Sigma(hh)) 
-    pcarslt=pca.transform(Sigma(hh))
-    x0=min(pcarslt[:,0]);x1=max(pcarslt[:,0])
-    y0=min(pcarslt[:,1]);y1=max(pcarslt[:,1])
-    z0=min(pcarslt[:,2]);z1=max(pcarslt[:,2])
-    if prnt==True: print('obtain snowdist')
-    for m in co(0,M):
-        if prnt==True: print('\r'+str(m),'/'+str(M),sep='',end='')
-        Σresult[m]=Sigma(hh,τmax=τlist[m])
-        pca4vis3d(Σresult[m],nodename=nodename,groupindex=groupindex,
-            title='τ='+str(τlist[m]),
-            figname=figname+str(m+1),figsize=figsize,dpi=dpi,
-            cex=cex,text=text,fade=fade,
-            xlim=(x0,x1),ylim=(y0,y1),zlim=(z0,z1))      
-    if prnt==True: print('\n'+'end')    
+# def pca4msvis3d(hstresult,τlist,
+#               nodename=None,groupindex=None,
+#               figname='temp',figsize=(1,1),dpi=1,cex=1,text=None,fade=1,
+#               prnt=False,logscale=(False,False,False)): # size=(size of obs representation, size of text which represent obs index)
+#     Σresult=τlist.copy()
+#     hh=hmat(hstresult)
+#     M=len(τlist)
+#     from sklearn.decomposition import PCA 
+#     pca=PCA(n_components=3) # PCA start 
+#     pca.fit(Sigma(hh)) 
+#     pcarslt=pca.transform(Sigma(hh))
+#     x0=min(pcarslt[:,0]);x1=max(pcarslt[:,0])
+#     y0=min(pcarslt[:,1]);y1=max(pcarslt[:,1])
+#     z0=min(pcarslt[:,2]);z1=max(pcarslt[:,2])
+#     if prnt==True: print('obtain snowdist')
+#     for m in np.arange(0,M):
+#         if prnt==True: print('\r'+str(m),'/'+str(M),sep='',end='')
+#         Σresult[m]=Sigma(hh,τmax=τlist[m])
+#         pca4vis3d(Σresult[m],nodename=nodename,groupindex=groupindex,
+#             title='τ='+str(τlist[m]),
+#             figname=figname+str(m+1),figsize=figsize,dpi=dpi,
+#             cex=cex,text=text,fade=fade,
+#             xlim=(x0,x1),ylim=(y0,y1),zlim=(z0,z1))      
+#     if prnt==True: print('\n'+'end')    
 
